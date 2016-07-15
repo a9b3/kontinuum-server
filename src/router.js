e172809642913716e6048f9f051c90c11522d34c
Sam Lau <esayemm@gmail.com>
2016-07-15 01:16:31 -0700

    require auth for deploy route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
 import index from './controllers/index.js'
+import deploy from './controllers/deploy.js'
+import keyAuth from './middleware/key-auth.js'
 
 const router = new Router()
 
 router.get(`/`, index)
+router.use(`/deploy`, keyAuth, deploy)
 
 export default router
